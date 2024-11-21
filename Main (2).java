5d64d44484eb7df35e2003f4029b7f3da7d289ba
PIYUSH SINGH <singhpiyush1403@gmail.com>
2024-11-21 12:41:23 +0530

    Add files via upload

---
+++ Main (2).java
@@ -0,0 +1,30 @@
+// public class conditional {
+// public static void main(String[] args){
+// int age = 90;
+// if(age>18){
+//  System.out.println("you can drive");
+// }
+//  else {
+     
+//      System.out.println("you cannot drive");
+     
+//         }
+//     }
+// }
+
+
+
+
+public class Main{
+    public static void main(String[]args){
+        boolean a = true;
+        boolean b = false;
+        boolean c = true;
+        if(a && b && c){
+            System.out.println("yes");  //logical and
+        }
+            else{
+                System.out.println("no");
+            }
+        }
+    }
